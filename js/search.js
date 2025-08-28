10c61fdcfba672a0f733b8cf8d3db8283e3e7888
Kadir <admh.gspb@gmail.com>
2025-08-28 10:38:07 +0200

    Implémentation recherche avec boucle for

--- js/search.js
+++ js/search.js
@@ -22,23 +22,3 @@ export function searchWithForLoop(query, recipes) {
 
   return filteredRecipes;
 }
-
-// Version 2 : recherche avec des méthodes fonctionnelles (filter, some)
-export function searchWithFunctionalMethods(query, recipes) {
-  const lowerCaseQuery = query.toLowerCase();
-
-  return recipes.filter((recipe) => {
-    const { name, description, ingredients } = recipe;
-
-    // On vérifie si la requête est dans le nom, les ingrédients ou la description
-    const foundInName = name.toLowerCase().includes(lowerCaseQuery);
-    const foundInDescription = description
-      .toLowerCase()
-      .includes(lowerCaseQuery);
-    const foundInIngredients = ingredients.some((ingredient) =>
-      ingredient.ingredient.toLowerCase().includes(lowerCaseQuery)
-    );
-
-    return foundInName || foundInDescription || foundInIngredients;
-  });
-}
